7363ce1bd4d43ee6cc8167cb45b043a9d981e621
ivanwonder <ivanwonder@outlook.com>
2018-06-02 20:56:37 +0800

    add BST test

--- test/BST.spec.js
+++ test/BST.spec.js
@@ -1,22 +1,94 @@
 import {BST} from '../algorithm/BST';
-import {getTreeNodeNumber} from '../lib/testBST';
+import {getTreeNodeNumber, isRBTree} from '../lib/testBST';
 const assert = require('assert');
 
+function theArrayValueIsAllSame (ar) {
+  let flag = true;
+  let number = null;
+  ar.forEach(function (value) {
+    if (number === null) {
+      number = value;
+    } else {
+      if (value !== number) {
+        flag = false;
+      }
+    }
+  });
+  return flag;
+}
+
+function testRBTree (tree) {
+  const result = isRBTree(tree);
+  if (theArrayValueIsAllSame(result.leafNodeBlackNumber) && !result.nodeRightIsRed) {
+    assert.ok(true);
+  } else {
+    console.log(result);
+    assert.fail('wrong tree');
+  }
+}
+
 describe('BST', function () {
-  const nodeSet = new Set();
-  let node;
+  const treeOne = [5];
+  const treeThree = [2, 1, 3];
+  const treeFour = [2, 1, 3, 4, 5];
+
   beforeEach(function () {
+  })
+
+  it('test random BST', function () {
     let times = 10;
-    let bst = new BST();
+    const bst = new BST();
+    const nodeSet = new Set();
     while (times--) {
       const key = bst.getRandom(times * 10);
       nodeSet.add(key);
       bst.put(key, key);
     }
-    node = bst.head;
+    const node = bst.head;
+
+    assert.deepEqual(nodeSet.size, getTreeNodeNumber(node));
+    testRBTree(bst.head);
   })
 
-  it('test BST node number', function () {
-    assert.deepEqual(nodeSet.size, getTreeNodeNumber(node));
-  })
+  it('build one node tree and delete it', function () {
+    const tree = new BST();
+    treeOne.forEach((value) => {
+      tree.put(value, value);
+    });
+    assert.equal(getTreeNodeNumber(tree.head), 1, 'the node number of tree must be one');
+    tree.delete(treeOne[0]);
+    assert.equal(tree.head, null, 'the tree must be null');
+  });
+
+  it('test the deleteMin and deleteMax', function () {
+    const tree = new BST();
+    treeThree.forEach(function (value) {
+      tree.put(value, value);
+    });
+    tree.deleteMin();
+    testRBTree(tree.head);
+    tree.deleteMax();
+    assert.equal(tree.head.key, 2);
+    tree.deleteMax();
+    assert.equal(tree.head, null, 'the tree should be null');
+    tree.put(1, 1);
+    tree.deleteMin();
+    assert.equal(tree.head, null, 'the tree must be null');
+  });
+
+  it('test the delete', function () {
+    const tree = new BST();
+    treeFour.forEach(function (value) {
+      tree.put(value, value);
+    });
+    tree.delete(3);
+    testRBTree(tree.head);
+    tree.delete(2);
+    testRBTree(tree.head);
+    assert.equal(getTreeNodeNumber(tree.head), 3, 'the node number of tree must be 3');
+    tree.delete(1);
+    tree.delete(4);
+    tree.delete(5);
+    assert.equal(tree.head, null, 'the tree must be null');
+  });
 })
