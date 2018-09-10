aa952c4b2db7fa48d15f79f8a0704cd81f8f4538
ivanwonder <ivanwonder@outlook.com>
2018-09-10 21:53:57 +0800

    add test instance

--- test/PQ.spec.js
+++ test/PQ.spec.js
@@ -6,8 +6,46 @@ describe('priority queue', function () {
    * @type {Array<>}
    */
   var testData;
+  var compareAble;
   beforeEach(function () {
     testData = [1, 2, 3, 4, 5];
+
+    function compareTo(data) {
+      if (this.value > data.value) {
+        return 1;
+      }
+
+      if (this.value < data.value) {
+        return -1;
+      }
+
+      if (this.value === data.value) {
+        return 0;
+      }
+    }
+
+    compareAble = [
+      {
+        value: 1,
+        compareTo
+      },
+      {
+        value: 2,
+        compareTo
+      },
+      {
+        value: 3,
+        compareTo
+      },
+      {
+        value: 4,
+        compareTo
+      },
+      {
+        value: 5,
+        compareTo
+      }
+    ]
   })
 
   it('test MinPQ', function () {
@@ -24,6 +62,25 @@ describe('priority queue', function () {
     }
     while (!minPQ.isEmpty()) {
       _res.push(minPQ.delMin());
+    }
+
+    assert.deepEqual(_res, [2, 1, 3, 4]);
+  })
+
+  it('test compareable data with MinPQ', function () {
+    let _res = [];
+    let minPQ = new MinPQ();
+    minPQ.insert(compareAble.pop());
+    assert.equal(minPQ.delMin().value, 5);
+    assert.equal(minPQ.isEmpty(), true);
+    while (compareAble.length) {
+      minPQ.insert(compareAble.pop());
+      if (compareAble.length === 1) {
+        _res.push(minPQ.delMin().value)
+      }
+    }
+    while (!minPQ.isEmpty()) {
+      _res.push(minPQ.delMin().value);
     }
 
     assert.deepEqual(_res, [2, 1, 3, 4]);
