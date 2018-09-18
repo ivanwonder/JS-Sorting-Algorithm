852b2bb798cd1caa7279ad5e56f6bc74da907510
ivan <ivanwonder@outlook.com>
2018-09-18 11:42:35 +0800

    update dependence

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,7 @@
 const merge = require('webpack-merge')
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+// const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
-  plugins: [
-    new UglifyJSPlugin()
-  ]
+  mode: "production"
 })
